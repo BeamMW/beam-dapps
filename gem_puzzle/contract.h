22f8bc80208bed0ee5e97dee484990830e85d3f6
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2021-08-13 12:55:00 +0300

    Add new_game function to gem_puzzle shader

---
+++ gem_puzzle/contract.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace GemPuzzle {
+	// SID: 
+	static const ShaderID s_SID = {};
+
+#pragma pack(push, 1)
+
+	struct NewGameParams {
+		static const uint32_t METHOD = 2;
+		// TODO: add params
+	};
+
+#pragma pack(pop)
+}
