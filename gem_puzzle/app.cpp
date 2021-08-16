651e81563150b400d7bc38a8f092eda2388ba15f
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2021-08-16 20:59:50 +0300

    Add check_solution function

--- gem_puzzle/app.cpp
+++ gem_puzzle/app.cpp
@@ -1,7 +1,12 @@
 #include "Shaders/common.h"
 #include "contract.h"
 
+#include <algorithm>
 #include <random>
+
+#define SOLUTION_BUF_SIZE	8192
+#define BOARD_SIZE			4
+#define PERMUTATION_LEN		15
 
 void On_action_new_game(const ContractID& cid)
 {
@@ -26,6 +31,153 @@ void On_action_new_game(const ContractID& cid)
 
 	Env::DocAddNum64("permutation", permutation_num);
 	// TODO: add game_number
+}
+
+bool check_solution(uint64_t permutation_num, const char* solution, uint32_t& moves_num)
+{
+	class Board {
+	public:
+		enum Moves { RIGHT, LEFT, UP, DOWN, CLOCKWISE, COUNTERCLOCKWISE };
+
+		Board(uint64_t permutation_num)
+		{
+			bool used[PERMUTATION_LEN + 1];
+			std::fill(used, used + PERMUTATION_LEN + 1, false);
+			for (size_t i = 0; i < BOARD_SIZE; ++i) {
+				for (size_t j = 0; j < BOARD_SIZE; ++j) {
+					if (i * BOARD_SIZE + j == 15) { // empty block
+						board[i][j] = 0;
+					} else {
+						uint64_t factorial = static_cast<uint64_t>(tgamma(PERMUTATION_LEN - (i * BOARD_SIZE + j)));
+						auto numbers_before = permutation_num / factorial;
+						permutation_num %= factorial;
+						size_t free_numbers = 0;
+						for (size_t k = 1; k <= PERMUTATION_LEN; ++k) {
+							if (!used[k]) {
+								++free_numbers;
+								if (free_numbers == numbers_before + 1) {
+									board[i][j] = k;
+									used[k] = true;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		bool is_solved()
+		{
+			uint8_t cur_value = 1;
+			bool result = true;
+			for (size_t i = 0; i < BOARD_SIZE; ++i)
+				for (size_t j = 0; j < BOARD_SIZE; ++j) {
+					if (i * BOARD_SIZE + j == PERMUTATION_LEN)
+						cur_value = 0;
+					result &= (board[i][j] == cur_value);
+					++cur_value;
+				}
+
+			return result;
+		}
+
+		void move(Moves m)
+		{
+			auto old_empty_cell = empty_cell;
+			const int8_t dx[] = { 1, -1, 0, 0 };
+			const int8_t dy[] = { 0, 0, -1, 1 };
+
+			// TODO: add error on invalid moves
+
+			switch (m) {
+			case RIGHT: case LEFT: case UP: case DOWN:
+				empty_cell.x += dx[static_cast<uint8_t>(m)];
+				empty_cell.y += dy[static_cast<uint8_t>(m)];
+				std::swap(board[old_empty_cell.y][old_empty_cell.x], board[empty_cell.y][empty_cell.x]);
+				break;
+			case CLOCKWISE:
+				for (size_t i = 0; i < BOARD_SIZE / 2; ++i)
+					for (size_t j = i; j < BOARD_SIZE - i - 1; ++j) {
+						std::swap(board[i][j], board[j][BOARD_SIZE - i - 1]);
+						std::swap(board[i][j], board[BOARD_SIZE - i - 1][BOARD_SIZE - j - 1]);
+						std::swap(board[i][j], board[BOARD_SIZE - j - 1][i]);
+					}
+				std::swap(empty_cell.x, empty_cell.y);
+				empty_cell.x = BOARD_SIZE - empty_cell.x - 1;
+				break;
+			case COUNTERCLOCKWISE:
+				for (size_t i = 0; i < BOARD_SIZE / 2; ++i)
+					for (size_t j = i; j < BOARD_SIZE - i - 1; ++j) {
+						std::swap(board[i][j], board[BOARD_SIZE - j - 1][i]);
+						std::swap(board[i][j], board[BOARD_SIZE - i - 1][BOARD_SIZE - j - 1]);
+						std::swap(board[i][j], board[j][BOARD_SIZE - i - 1]);
+					}
+				std::swap(empty_cell.x, empty_cell.y);
+				empty_cell.y = BOARD_SIZE - empty_cell.y - 1;
+				break;
+			default:
+				break;
+			}
+		}
+
+	private:
+		uint8_t board[BOARD_SIZE][BOARD_SIZE];
+		struct {
+			uint8_t x;
+			uint8_t y;
+		} empty_cell;
+	} board(permutation_num);
+
+	for (auto i = 0; solution[i] != '\0'; ++i) {
+		Board::Moves cur_move;
+		switch (toupper(solution[i])) {
+		case 'R':
+			cur_move = Board::RIGHT;
+			break;
+		case 'L':
+			cur_move = Board::LEFT;
+			break;
+		case 'U':
+			cur_move = Board::UP;
+			break;
+		case 'D':
+			cur_move = Board::DOWN;
+			break;
+		case 'F':
+			cur_move = Board::CLOCKWISE;
+			break;
+		case 'B':
+			cur_move = Board::COUNTERCLOCKWISE;
+			break;
+		default:
+			// TODO: add error
+			return false;
+		}
+		board.move(cur_move);
+	}
+	return board.is_solved();
+}
+
+void On_action_check_solution(const ContractID& cid)
+{
+	uint32_t gameID = 0;
+	Env::DocGetNum32("gameID", &gameID);
+
+	char solution[SOLUTION_BUF_SIZE];
+	Env::DocGetText("solution", solution, sizeof(solution));
+
+	uint64_t permutation_num = 0;
+	// TODO: read permutation and height by gameID
+	uint32_t moves_num = 0;
+	Env::DocAddGroup("");
+	Env::DocAddText("verdict", check_solution(permutation_num, solution, moves_num) ? "win" : "lose");
+	Env::DocAddNum32("moves", moves_num);
+
+	// Env::get_Height()
+	// TODO: calculate time
+
+	//Env::DocAddNum64("time (min)", time);
+	Env::DocCloseGroup();
 }
 
 export void Method_0()
