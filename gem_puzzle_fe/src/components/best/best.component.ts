c49404654d75652a45a9901e7151ded0388202a2
JenkJS <jenkjobs@gmail.com>
2021-09-15 11:59:25 +0300

    gem_puzzle_fe: reStyle main, liderboard, button, add icon

--- gem_puzzle_fe/src/components/best/best.component.ts
+++ gem_puzzle_fe/src/components/best/best.component.ts
@@ -7,14 +7,13 @@ import './best.scss';
 
 export class Best extends BaseComponent {
   constructor(top: any []) {
-    super(Tags.DIV, ['top']);
+    super(Tags.TABLE, ['top']);
     // this.sort(top)
     this.initTableTime(top);
   }
 
   initTableTime = (top: any []):void => {
     this.removeAll();
-    const best = new BaseComponent(Tags.TABLE, ['top__best']);
     const { pKey } = AppStateHandler.getState();
     const trHeader = new BaseComponent(Tags.TR);
     const thPos = new BaseComponent(Tags.TH);
@@ -30,14 +29,27 @@ export class Best extends BaseComponent {
     thPermutation.element.textContent = 'Permutation';
     thTime.element.classList.add('active', 'use');
     thMoves.element.classList.add('use');
-    best.append(trHeader);
+    this.append(trHeader);
     trHeader.append(thPos, thAcc, thTime, thMoves, thPermutation);
     top?.sort((obj1: any, obj2: any) => obj1.time - obj2.time)
       .forEach((el: any, idx: any):void => {
         const tdN = new BaseComponent(Tags.TD);
         const tr = new BaseComponent(Tags.TR);
         tr.element.classList.add('tRow');
-        tdN.element.textContent = idx + 1;
+        tdN.element.innerHTML = `<span>${idx + 1}</span>`;
+        if (idx === 0) {
+          tr.element.classList.add('leader');
+          tdN.element.innerHTML += `<svg width="29" height="30" xmlns="http://www.w3.org/2000/svg">
+          <g fill-rule="nonzero" fill="none">
+              <circle fill="#FFD321" cx="13.748" cy="11.911" r="11.911"/>
+              <g fill="#FFE470">
+                  <path d="M4.098 18.88 0 25.984l4.783-.254L6.957 30l3.791-6.573a11.9 11.9 0 0 1-6.65-4.547zM23.421 18.844a11.903 11.903 0 0 1-6.632 4.571L20.593 30l2.173-4.27 4.783.254-4.128-7.14z"/>
+              </g>
+              <circle fill="#FF9F00" cx="13.748" cy="11.911" r="8.634"/>
+              <path d="M19.665 10.978a.451.451 0 0 0-.248-.767l-3.407-.496a.444.444 0 0 1-.337-.248L14.15 6.378a.45.45 0 0 0-.81 0l-1.517 3.089a.469.469 0 0 1-.337.248l-3.407.496a.451.451 0 0 0-.248.767l2.462 2.404a.455.455 0 0 1 .13.396l-.579 3.39a.45.45 0 0 0 .656.472l3.047-1.6a.44.44 0 0 1 .42 0l3.047 1.6a.452.452 0 0 0 .655-.473l-.584-3.39a.444.444 0 0 1 .13-.395l2.45-2.404z" fill="#FFF"/>
+          </g>
+      </svg>`;
+        }
         tr.append(tdN);
 
         const entries = Object.entries(el);
@@ -51,8 +63,8 @@ export class Best extends BaseComponent {
             }
             tr.append(td);
           }
-          best.append(tr);
-          this.append(best);
+          this.append(tr);
+          // this.append(best);
         });
       });
 
@@ -66,7 +78,6 @@ export class Best extends BaseComponent {
 
   initTableMove = (top: any):void => {
     this.removeAll();
-    const best = new BaseComponent(Tags.TABLE, ['top__best']);
     const { pKey } = AppStateHandler.getState();
     const trHeader = new BaseComponent(Tags.TR);
     const thPos = new BaseComponent(Tags.TH);
@@ -75,33 +86,49 @@ export class Best extends BaseComponent {
     const thMoves = new BaseComponent(Tags.TH);
     const thPermutation = new BaseComponent(Tags.TH);
     trHeader.classList.add('thHeader');
-    thPos.element.textContent = '№';
-    thAcc.element.textContent = 'Account';
+    thPos.element.textContent = 'RANK';
+    thAcc.element.textContent = 'PUBLIC KEY';
     thTime.element.textContent = 'Time';
     thMoves.element.textContent = 'Moves';
     thPermutation.element.textContent = 'Permutation';
-    thMoves.element.classList.add('active', 'use');
-    thTime.element.classList.add('use');
-    best.append(trHeader);
+    thTime.element.classList.add('active', 'use');
+    thMoves.element.classList.add('use');
+    this.append(trHeader);
     trHeader.append(thPos, thAcc, thTime, thMoves, thPermutation);
     top?.sort((obj1: any, obj2: any) => obj1.moves - obj2.moves)
-      .forEach((el: any, idx: any) => {
+      .forEach((el: any, idx: any):void => {
         const tdN = new BaseComponent(Tags.TD);
         const tr = new BaseComponent(Tags.TR);
+        tr.element.classList.add('tRow');
         tdN.element.textContent = idx + 1;
+        if (idx === 0) {
+          tr.element.classList.add('leader');
+          tdN.element.innerHTML += `<svg width="29" height="30" xmlns="http://www.w3.org/2000/svg">
+          <g fill-rule="nonzero" fill="none">
+              <circle fill="#FFD321" cx="13.748" cy="11.911" r="11.911"/>
+              <g fill="#FFE470">
+                  <path d="M4.098 18.88 0 25.984l4.783-.254L6.957 30l3.791-6.573a11.9 11.9 0 0 1-6.65-4.547zM23.421 18.844a11.903 11.903 0 0 1-6.632 4.571L20.593 30l2.173-4.27 4.783.254-4.128-7.14z"/>
+              </g>
+              <circle fill="#FF9F00" cx="13.748" cy="11.911" r="8.634"/>
+              <path d="M19.665 10.978a.451.451 0 0 0-.248-.767l-3.407-.496a.444.444 0 0 1-.337-.248L14.15 6.378a.45.45 0 0 0-.81 0l-1.517 3.089a.469.469 0 0 1-.337.248l-3.407.496a.451.451 0 0 0-.248.767l2.462 2.404a.455.455 0 0 1 .13.396l-.579 3.39a.45.45 0 0 0 .656.472l3.047-1.6a.44.44 0 0 1 .42 0l3.047 1.6a.452.452 0 0 0 .655-.473l-.584-3.39a.444.444 0 0 1 .13-.395l2.45-2.404z" fill="#FFF"/>
+          </g>
+      </svg>`;
+        }
         tr.append(tdN);
+
         const entries = Object.entries(el);
         entries.forEach((key) => {
           const td = new BaseComponent(Tags.TD);
           if (key[1]) {
             td.element.textContent = `${key[1]}`;
             if (key[1] === pKey) {
-              tr.style.background = 'rgba(62, 102, 251, .7)';
+              tr.style.backgroundColor = 'rgba(0, 191, 74, 0.3)';
+              tr.style.border = 'solid 2px #00bf4a';
             }
             tr.append(td);
           }
-          best.append(tr);
-          this.append(best);
+          this.append(tr);
+          // this.append(best);
         });
       });
 
--- gem_puzzle_fe/src/components/header/header.component.ts
+++ gem_puzzle_fe/src/components/header/header.component.ts
@@ -4,43 +4,60 @@ import BaseComponent from '../base/base.component';
 import './header.scss';
 // import { boardSchemeMaker } from '../../utils/string_handlers';
 import InfoBLock from './infoblock.component';
-import iconFunt from '../../assets/icon/icon-funts-coins-stack.svg';
 
 export default class Header extends BaseComponent {
   constructor() {
     super(Tags.DIV, ['header']);
-    const {
-      reward
-    } = AppStateHandler.getState();
-    const rewardBlock = new InfoBLock({
-      key: 'reward',
-      title: `<svg width="34" height="31" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fill-rule="evenodd">
-          <path fill="#008A36" d="M16.982.5 4.324 22.377 16.982 30.5l12.657-8.123z"/>
-          <path fill="#00FF64" d="M16.982.5 4.324 22.377l12.658 1.434 12.657-1.434z"/>
-          <path fill="#00BF4A" d="m16.982.5 12.987 7.35-.33 14.527z"/>
-          <path fill="#00BF4A" d="M16.987.5 4 7.85l.33 14.527z"/>
-      </g>
-  </svg>`,
-      value: reward,
-      after: 'FUNT'
-    });
+    const { reward } = AppStateHandler.getState();
     const logoBlock = new InfoBLock({
       key: 'logo',
-      title: `<svg width="34" height="31" xmlns="http://www.w3.org/2000/svg">
+      title: `<svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
       <g fill="none" fill-rule="evenodd">
-          <path fill="#008A36" d="M16.982.5 4.324 22.377 16.982 30.5l12.657-8.123z"/>
-          <path fill="#00FF64" d="M16.982.5 4.324 22.377l12.658 1.434 12.657-1.434z"/>
-          <path fill="#00BF4A" d="m16.982.5 12.987 7.35-.33 14.527z"/>
-          <path fill="#00BF4A" d="M16.987.5 4 7.85l.33 14.527z"/>
+      <path d="M35.714 0A4.286 4.286 0 0 1 40 4.286v31.428A4.286 4.286 0 0 1 35.714 40H4.286A4.286 4.286 0 0 1 0 35.714V4.286A4.286 4.286 0 0 1 4.286 0h31.428zm0 2.143H4.286c-1.114 0-2.03.85-2.133 1.936l-.01.207v31.428c0 1.114.85 2.03 1.936 2.133l.207.01h31.428c1.114 0 2.03-.85 2.133-1.936l.01-.207V4.286c0-1.114-.85-2.03-1.936-2.133l-.207-.01z" fill="#00FF64" fill-rule="nonzero"/>
+      <path d="M33.12 0A2.88 2.88 0 0 1 36 2.88v30.24A2.88 2.88 0 0 1 33.12 36H2.88A2.88 2.88 0 0 1 0 33.12V2.88A2.88 2.88 0 0 1 2.88 0h30.24zm0 2.16H2.88a.72.72 0 0 0-.708.59l-.012.13v30.24a.72.72 0 0 0 .59.708l.13.012h30.24a.72.72 0 0 0 .708-.59l.012-.13V2.88a.72.72 0 0 0-.59-.708l-.13-.012z" fill="#00FF64" fill-rule="nonzero"/>
+      <rect stroke="#00FF64" stroke-width="2" fill="#00FF64" x="5" y="20" width="11" height="11" rx="2"/>
+      <rect stroke="#00FF64" stroke-width="2" fill="#00FF64" x="20" y="20" width="11" height="11" rx="2"/>
+      <rect stroke="#FAFF7A" stroke-width="2" fill="#FAFF7A" x="20" y="5" width="11" height="11" rx="2"/>
       </g>
-  </svg>`,
-      value: ' ',
-      after: 'GEM-PUZLLE'
+      </svg>
+      `,
+      value: '',
+      after: 'GEM-PUZZLE',
+      before: ' '
     });
-    this.append(
-      logoBlock,
-      rewardBlock
-    );
+    this.append(logoBlock);
+    if (reward !== 0) {
+      const rewardBlock = new InfoBLock({
+        key: 'reward',
+        title: `<svg width="34" height="31" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fill-rule="evenodd">
+        <path fill="#008A36" d="M16.982.5 4.324 22.377 16.982 30.5l12.657-8.123z"/>
+        <path fill="#00FF64" d="M16.982.5 4.324 22.377l12.658 1.434 12.657-1.434z"/>
+        <path fill="#00BF4A" d="m16.982.5 12.987 7.35-.33 14.527z"/>
+        <path fill="#00BF4A" d="M16.987.5 4 7.85l.33 14.527z"/>
+    </g>
+</svg>`,
+        value: reward,
+        after: 'FUNT',
+        before: ' '
+      });
+      this.append(rewardBlock);
+    } else {
+      const rewardBlock = new InfoBLock({
+        key: 'reward',
+        title: ` <svg width="34" height="31" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fill-rule="evenodd">
+        <path fill="#008A36" d="M16.982.5 4.324 22.377 16.982 30.5l12.657-8.123z"/>
+        <path fill="#00FF64" d="M16.982.5 4.324 22.377l12.658 1.434 12.657-1.434z"/>
+        <path fill="#00BF4A" d="m16.982.5 12.987 7.35-.33 14.527z"/>
+        <path fill="#00BF4A" d="M16.987.5 4 7.85l.33 14.527z"/>
+    </g>
+</svg> `,
+        value: reward,
+        before: 'CLAIM ',
+        after: ' FUNT'
+      });
+      this.append(rewardBlock);
+    }
   }
 }
