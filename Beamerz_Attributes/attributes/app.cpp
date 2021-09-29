5117468fc19c6429ae750e5506dc63e149a1ace9
Georgy <georgeadamovich2@gmail.com>
2021-09-29 15:54:12 +0200

    Update app.cpp
    Defining user's possibilities

--- Beamerz_Attributes/attributes/app.cpp
+++ Beamerz_Attributes/attributes/app.cpp
@@ -29,3 +29,55 @@
     macro(admin, set_user);
     macro(admin, view_balance);
     macro(admin, decide);
+
+#define Beamerz_user_view(macro)
+macro (ContractID, cid)
+#define Beamerz_user_get_key(macro)
+macro (ContractID, cid)
+#define Beamerz_user_Upload(macro)
+macro (ContractID, cid)
+
+#define BeamerzRole_user(macro)
+macro (user, view);
+macro(user, get_key);
+macro(user, upload);
+
+#define Beamerz_user_view_item(macro);
+macro (ContractID, cid);
+macro(Beamerz::Attribute::ID, id)
+
+#define Beamerz_user_view_all(macro);
+macro (ContractID, cid)
+
+#define Beamerz_user_set_price(macro);
+macro (ContractID, cid);
+macro (Beamerz:Attribute::, id);
+macro (Amount, amount);
+macro (AssetID, aid)
+
+#define Beamerz_user_buy(macro);
+macro (ContractID, cid);
+macro (Beamerz::Attribute:ID, id)
+
+#define Beamerz_user_view_balance(macro)
+macro (ContractID, cid)
+
+#define Beamerz_user_withdraw(macro);
+macro (ContractID, cid);
+macro (uint_32_t, nMaxCount);
+
+#define BeamerzRole_user(macro);
+    macro(user, view_item); 
+    macro(user, view_all);
+    macro(user, download);
+    macro(user, set_price);
+    macro(user, buy);
+    macro(user, view_balance);
+    macro(user, withdraw);
+
+
+#define BeamerzRole_All(macro);
+    macro (manager);
+    macro (admin);
+    macro (user);
+
