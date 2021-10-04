5d8b1a8af96e5d8e12fd5b4dfd09f953be6d1380
Nikita Shlyago <85184722+nikkitashl@users.noreply.github.com>
2021-10-05 00:05:25 +0300

    Add parts of gallery to nft-gallery

--- nft-generator/contract.cpp
+++ nft-generator/contract.cpp
@@ -1,16 +1,122 @@
 #include "contract.h"
+#include "../Math.h"
 
-BEAM_EXPORT void Ctor(void*) {
-
-}
-
-BEAM_EXPORT void Dtor(void*) {
+BEAM_EXPORT void Ctor(void *) {
 
 }
 
-BEAM_EXPORT void Method_2(const SaveNewSeed& request) {
-    Env::SaveVar_T(NFTGenerator::ComplexKeyWithSeed {
-            .key = request.key,
-            .seed = request.seed
-    }, request.holder);
+BEAM_EXPORT void Dtor(void *) {
+
+}
+
+BEAM_EXPORT void Method_2(const NFTGenerator::SaveNewSeed &request) {
+    Env::SaveVar_T(request.nft.seed, request.nft);
+}
+
+BEAM_EXPORT void Method_3(const NFTGenerator::SetPrice &r) {
+    NFTGenerator::NFT m;
+    Env::Halt_if(!Env::LoadVar_T(r.updated_nft.seed, m));
+
+    _POD_(m) = r.updated_nft;
+    Env::SaveVar_T(m.seed, m);
+
+    Env::AddSig(m.holder);
+}
+
+void PayoutMove(const NFTGenerator::Payout::Key &key, Amount val, bool bAdd) {
+    if (!val) {
+        return;
+    }
+
+    NFTGenerator::Payout po;
+    if (Env::LoadVar_T(key, po)) {
+        if (bAdd) {
+            Strict::Add(po.amount, val);
+        } else {
+            Strict::Sub(po.amount, val);
+
+            if (!po.amount) {
+                Env::DelVar_T(key);
+                return;
+            }
+        }
+    } else {
+        Env::Halt_if(!bAdd);
+        po.amount = val;
+    }
+
+    Env::SaveVar_T(key, po);
+}
+
+
+BEAM_EXPORT void Method_4(const NFTGenerator::Buy &r) {
+    NFTGenerator::NFT m;
+    Env::Halt_if(!Env::LoadVar_T(r.seed, m));
+
+    Env::Halt_if(!m.price.amount || (r.price.amount < m.price.amount));
+
+    Env::FundsLock(m.price.asset_id, r.price.amount);
+
+    NFTGenerator::Payout::Key pok;
+    pok.seed = r.seed;
+    pok.asset_id = m.price.asset_id;
+
+    _POD_(pok.user) = m.holder;
+    PayoutMove(pok, m.price.amount, true);
+
+    _POD_(pok.user) = r.buyer;
+    PayoutMove(pok, r.price.amount - m.price.amount, true);
+
+    _POD_(m.holder) = r.buyer;
+    _POD_(m.price.amount).SetZero(); // not for sale until new owner sets the price
+    Env::SaveVar_T(r.seed, m);
+}
+
+BEAM_EXPORT void Method_5(const NFTGenerator::Withdraw &r) {
+    PayoutMove(r.key, r.value, false);
+    Env::FundsUnlock(r.key.asset_id, r.value);
+    Env::AddSig(r.key.user);
+}
+
+BEAM_EXPORT void Method_6(const NFTGenerator::CheckPrepare &r) {
+    NFTGenerator::NFT m;
+    Env::Halt_if(!Env::LoadVar_T(r.seed, m));
+    Env::AddSig(m.holder);
+
+    if (m.nft_asset_id) {
+        // destroy it
+        Env::Halt_if(!Env::AssetDestroy(m.nft_asset_id));
+        m.nft_asset_id = 0;
+    } else {
+        // 1st call. Don't checkout, only prepare
+        static const char szMeta[] = "STD:SCH_VER=1;N=NFT-Gallery NFT;SN=Gall;UN=GALL;NTHUN=unique";
+        m.nft_asset_id = Env::AssetCreate(szMeta, sizeof(szMeta) - 1);
+    }
+
+    Env::SaveVar_T(r.seed, m);
+}
+
+BEAM_EXPORT void Method_7(const NFTGenerator::CheckOut &r) {
+    NFTGenerator::NFT m;
+    Env::Halt_if(!Env::LoadVar_T(r.seed, m) || !m.nft_asset_id);
+    Env::AddSig(m.holder);
+
+    Env::Halt_if(!Env::AssetEmit(m.nft_asset_id, 1, 1));
+    Env::FundsUnlock(m.nft_asset_id, 1);
+
+    _POD_(m.holder).SetZero();
+    _POD_(m.price).SetZero();
+
+    Env::SaveVar_T(r.seed, m);
+}
+
+BEAM_EXPORT void Method_8(const NFTGenerator::CheckIn &r) {
+    NFTGenerator::NFT m;
+    Env::Halt_if(!Env::LoadVar_T(r.seed, m) || !_POD_(m.holder).IsZero());
+
+    Env::FundsLock(m.nft_asset_id, 1);
+    Env::Halt_if(!Env::AssetEmit(m.nft_asset_id, 1, 0));
+
+    _POD_(m.holder) = r.user;
+    Env::SaveVar_T(r.seed, m);
 }
--- nft-generator/contract.h
+++ nft-generator/contract.h
@@ -5,38 +5,79 @@
 
 namespace NFTGenerator {
 
-    static const ShaderID SID = { 0x0e,0xfa,0xb3,0x7b,0x8c,0xe4,0x27,0x0f,0x36,0x85,0x8c,0x0e,0x45,0x9b,0x09,0x5e,0x70,0x4d,0x0c,0x5e,0xb9,0xb1,0x1d,0xdc,0x68,0xa7,0x0a,0xb1,0x9e,0xf6,0x61,0xb4 };
-
-    static const ContractID gallery_CID = { 0x65,0x9a,0xb5,0x83,0x4c,0x97,0xe8,0x2c,0x13,0xe2,0xab,0x8b,0xdf,0xf4,0x6a,0xd3,0xd6,0x13,0x91,0x5d,0xb6,0x87,0xed,0x2a,0xf5,0x75,0xc8,0xda,0x0d,0xc6,0xbc,0x4a };
+    static const ShaderID SID = {0x0e, 0xfa, 0xb3, 0x7b, 0x8c, 0xe4, 0x27, 0x0f, 0x36, 0x85, 0x8c, 0x0e, 0x45, 0x9b,
+                                 0x09, 0x5e, 0x70, 0x4d, 0x0c, 0x5e, 0xb9, 0xb1, 0x1d, 0xdc, 0x68, 0xa7, 0x0a, 0xb1,
+                                 0x9e, 0xf6, 0x61, 0xb4};
 
 #pragma pack (push, 1)
 
-    struct ComplexKey {
-        AssetID aid;
-        ContractID cid;
+    struct Price {
+        Amount amount;
+        AssetID asset_id;
     };
 
-    struct ComplexKeyWithSeed {
-        ComplexKey key;
+    struct NFT {
         uint64_t seed;
+        PubKey holder;
+        Price price;
+        AssetID nft_asset_id;
     };
 
     struct SaveNewSeed {
-        static constexpr uint64_t
-        s_iMethod = 2;
+        static constexpr uint64_t s_iMethod = 2;
 
-        ComplexKey key;
-        uint64_t seed;
-        PubKey holder;
+        NFT nft;
     };
 
-    struct AddExhibit // struct from gallery to add pic
-    {
+    struct Payout {
+        struct Key {
+            PubKey user;
+            AssetID asset_id;
+            uint64_t seed;
+        };
+
+        NFT nft;
+        Amount amount;
+    };
+
+    struct SetPrice {
         static const uint32_t s_iMethod = 3;
 
-        PubKey m_pkArtist;
-        uint32_t m_Size;
-        // followed by the data
+        NFT updated_nft;
+    };
+
+    struct Buy {
+        static const uint32_t s_iMethod = 4;
+
+        uint64_t seed;
+        PubKey buyer;
+        Price price;
+    };
+
+    struct Withdraw {
+        static const uint32_t s_iMethod = 5;
+
+        Payout::Key key;
+        Amount value;
+    };
+
+    struct CheckPrepare {
+        static const uint32_t s_iMethod = 6;
+
+        uint64_t seed;
+    };
+
+    struct CheckOut {
+        static const uint32_t s_iMethod = 7;
+
+        uint64_t seed;
+    };
+
+    struct CheckIn {
+        static const uint32_t s_iMethod = 8;
+
+        uint64_t seed;
+        PubKey user;
     };
 
 #pragma pack (pop)
